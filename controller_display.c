48b6449944f52ac146b67549cd8095509383934f
abropez <abropez@gmail.com>
2013-04-28 00:38:09 -0700

    display controller, up to date w/touch & graphics working

--- controller_display.c
+++ controller_display.c
@@ -1,4 +1,4 @@
-/*
+ /*
  *  controller_display.c
  *  
  *
@@ -7,21 +7,25 @@
  *
  */
 #include <avr/io.h>
-#include <avr/pgmspace.h> 
+//#include <avr/pgmspace.h> 
 #include "adafruit335.c"
 #include "controller_display.h"
+#include "lgserial.h"
+#include "adatouch.cpp"
 
 Controller::Controller()
 {
-  
+	
 }
 
 void Controller::setup()
-{
+{	
+	LGSerial::init(); 
+	LGSerial::put("Hello World!"); 
 	DDRB |= LCD_Data_B; 
 	DDRD |= 0xffff; 
-	PORTC |= LCD_Bits_C; 
-	DDRC |= LCD_Bits_C; 
+	//PORTC |= LCD_Bits_C; 
+	//DDRC |= LCD_Bits_C; 
 	
 	initialize(); 
 	
@@ -33,6 +37,49 @@ void Controller::setup()
 }
 
 void Controller::loop()
-{
+{	
 	homeMenu(); 
+	while (1)
+	{
+	sleep(100); 
+	DDRC = 0x0;  
+	DDRC = 0x5; 
+	TouchScreen ts = TouchScreen(10,10,10,10,10); 
+	LGSerial::put("YM:"); 
+		int y = ts.readTouchYM(); 
+	LGSerial::print(y); 
+	
+	//DDRC |= 0xf; 
+	DDRC = 0x0;
+	DDRC = 0xa; 
+	LGSerial::put("XM:"); 
+		int x  = ts.readTouchXM(); 
+	LGSerial::print(x); 
+		x = ts.readTouchXM(); 
+	sleep(100); 
+		
+		
+		 if (x > 30 && x<100 && y>60 && y < 130)
+		 {
+		   devicesMenu(); 
+			 homeMenu();
+		 }
+		 
+		 else if (y> 140 && y<220 && x>30 && x<100)
+		 {
+		settingsMenu();
+			 homeMenu();
+		 }
+		 else if (x>125 && x<215 &&y>60 && y<130)
+		 {
+			 statusMenu(); 
+			homeMenu();
+		 }
+		 else if (x>125 && x<215 && y>140 && y<220) 
+		 {
+		   scheduleMenu();
+			 homeMenu();
+		 }	
+		 
+	}
 }
